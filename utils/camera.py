33069cfdace27ddb4877f130720b388a955820fb
Mansouri <47946112+wissemnano@users.noreply.github.com>
2019-06-20 13:49:11 +0200

    fix a bug in this program
    fix a bug in the stream because the stream dosen't work with the new versions of sanic you must add await in stream(self, rsp)
    ::await rsp.write()

--- utils/camera.py
+++ utils/camera.py
@@ -21,7 +21,7 @@ class Camera():
 
     async def stream(self, rsp):
         async for frame in self.frames():
-            rsp.write(
+            await rsp.write(
                 b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
             )
